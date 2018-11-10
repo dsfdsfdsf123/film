50fde504bf217d95c18ade620fea66f44848c570
liugang <860721890@qq.com>
2018-11-11 00:14:27 +0800

    链接管理的界面，实现了添加和编辑功能

--- src/main/java/com/money/film/entity/Link.java
+++ src/main/java/com/money/film/entity/Link.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 public class Link {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
     @Column(length = 500)
