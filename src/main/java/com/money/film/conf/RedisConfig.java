589f4445c073799251d52e9a8b7e3ee4847a3a63
liugang <860721890@qq.com>
2018-11-11 23:09:24 +0800

    在网上看如何在springboot里面整合redis

---
+++ src/main/java/com/money/film/conf/RedisConfig.java
@@ -0,0 +1,108 @@
+package com.money.film.conf;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import javax.annotation.Resource;
+
+/**
+ * redis的配置类
+ * @author liugang
+ * @create 2018/11/11 22:10
+ **/
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Resource
+    private JedisConnectionFactory jedisConnectionFactory;
+
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(jedisConnectionFactory);
+        return builder.build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory ) {
+        //设置序列化
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        // 配置redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        // key序列化
+        redisTemplate.setKeySerializer(stringSerializer);
+        // value序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        // Hash key序列化
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        // Hash value序列化
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    /**
+     * 此内部类就是把yml的配置数据，进行读取，创建JedisConnectionFactory和JedisPool，以供外部类初始化缓存管理器使用
+     * 不了解的同学可以去看@ConfigurationProperties和@Value的作用
+     *
+     */
+    @ConfigurationProperties
+    class DataJedisProperties{
+        @Value("${spring.redis.host}")
+        private  String host;
+        @Value("${spring.redis.port}")
+        private  int port;
+        @Value("${spring.redis.timeout}")
+        private  int timeout;
+        @Value("${spring.redis.jedis.pool.max-idle}")
+        private int maxIdle;
+        @Value("${spring.redis.jedis.pool.max-wait}")
+        private long maxWaitMillis;
+
+        @Bean
+        JedisConnectionFactory jedisConnectionFactory() {
+            JedisConnectionFactory factory = new JedisConnectionFactory();
+            factory.setHostName(host);
+            factory.setPort(port);
+            factory.setTimeout(timeout);
+            return factory;
+        }
+        @Bean
+        public JedisPool redisPoolFactory() {
+            JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+            jedisPoolConfig.setMaxIdle(maxIdle);
+            jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+
+            JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout);
+            return jedisPool;
+        }
+    }
+
+
+}
---
+++ src/main/java/com/money/film/controller/RedisController.java
@@ -0,0 +1,43 @@
+package com.money.film.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.money.film.entity.WebSite;
+import com.money.film.repository.WebSiteRepository;
+import com.money.film.util.RedisOperator;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author liugang
+ * @create 2018/11/11 22:58
+ **/
+@RestController
+@RequestMapping("/testRedis")
+public class RedisController {
+
+    @Resource
+    private RedisOperator redisOperator;
+    @Resource
+    private WebSiteRepository webSiteRepository;
+
+    @RequestMapping("/set")
+    public String set(){
+        redisOperator.set("name","杨武");
+        redisOperator.incr("age",22);
+        WebSite webSite = new WebSite();
+        webSite.setId(5);
+        List<WebSite> list = webSiteRepository.findAll();
+        System.out.println(list);
+        redisOperator.set("json:list", JSON.toJSONString(list));
+        return "success";
+    }
+
+    @RequestMapping("/get")
+    public String get(){
+        return redisOperator.get("json:list");
+    }
+}
